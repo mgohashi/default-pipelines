adfcec8c0073a6a0224466c7cab6992e00e6db37
mohashi <mohashi@gmail.com>
2018-08-09 15:01:51 -0300

    .

--- create-build.sh
+++ create-build.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 oc process -f build-configs/build-pipeline.yml \
-    -p NAMESPACE='test-dev' -p APP_NAME='test-app' \
-    -p GIT_URL='http://app-test/git.git' \
+    -p NAMESPACE='test-dev' -p APP_NAME='api-client' \
+    -p GIT_URL='http://gogs-banestes-cicd-tools.apps.rh-consulting-br.com/banestes/api-client' \
     -p JENKINS_FILE='pipelines/Jenkinsfile-Build-Dev' \
     | oc apply -n test-dev -f -
 
 oc process -f build-configs/build-pipeline.yml \
-    -p NAMESPACE='test-hml' -p APP_NAME='test-app' \
-    -p GIT_URL='http://app-test/git.git' \
+    -p NAMESPACE='test-hml' -p APP_NAME='api-client' \
+    -p GIT_URL='http://gogs-banestes-cicd-tools.apps.rh-consulting-br.com/banestes/api-client' \
     -p JENKINS_FILE='pipelines/Jenkinsfile-Build-Hml' \
     | oc apply -n test-hml -f -
 
 oc process -f build-configs/build-pipeline.yml \
-    -p NAMESPACE='test-hml' -p APP_NAME='test-app' \
-    -p GIT_URL='http://app-test/git.git' \
+    -p NAMESPACE='test-hml' -p APP_NAME='api-client' \
+    -p GIT_URL='http://gogs-banestes-cicd-tools.apps.rh-consulting-br.com/banestes/api-client' \
     -p JENKINS_FILE='pipelines/Jenkinsfile-Build-Hotfix' \
     -p SUFIX='-hotfix' | oc apply -n test-hml -f -
 
 oc process -f build-configs/build-pipeline.yml \
-    -p NAMESPACE='test-prd' -p APP_NAME='test-app' \
-    -p GIT_URL='http://app-test/git.git' \
+    -p NAMESPACE='test-prd' -p APP_NAME='api-client' \
+    -p GIT_URL='http://gogs-banestes-cicd-tools.apps.rh-consulting-br.com/banestes/api-client' \
     -p JENKINS_FILE='pipelines/Jenkinsfile-Prd' \
     | oc apply -n test-prd -f -

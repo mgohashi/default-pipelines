dd912dcba66268b23139ce047a35bc0059c863dd
Marcelo Ohashi <mohashi@redhat.com>
2018-08-10 17:33:42 +0000

    removendo projeto cross

--- create-build.sh
+++ create-build.sh
@@ -4,4 +4,16 @@ oc process -f build-configs/build-pipeline.yml \
     -p NAMESPACE_BASE='rh-test' -p APP_NAME='api-client' \
     -p GIT_URL='https://gogs-cicd-tools.cloud.sfb/banestes/api-cliente.git' \
     -p JENKINS_FILE='pipelines/Jenkinsfile-Build' \
-    | oc apply -n rh-test-cross -f -
+    | oc apply -n rh-test-dev -f -
+
+oc process -f build-configs/build-pipeline.yml \
+    -p NAMESPACE_BASE='rh-test' -p APP_NAME='api-client' \
+    -p GIT_URL='https://gogs-cicd-tools.cloud.sfb/banestes/api-cliente.git' \
+    -p JENKINS_FILE='pipelines/Jenkinsfile-Build' \
+    | oc apply -n rh-test-hml -f -
+
+oc process -f build-configs/build-pipeline.yml \
+    -p NAMESPACE_BASE='rh-test' -p APP_NAME='api-client' \
+    -p GIT_URL='https://gogs-cicd-tools.cloud.sfb/banestes/api-cliente.git' \
+    -p JENKINS_FILE='pipelines/Jenkinsfile-Build' \
+    | oc apply -n rh-test -f -

88c7b6991023e92f607f8c0daafff761779a5ce2
Marcelo Ohashi <mohashi@redhat.com>
2018-08-13 13:36:32 +0000

    .

--- create-build.sh
+++ create-build.sh
@@ -4,16 +4,19 @@ oc process -f build-configs/build-pipeline.yml \
     -p NAMESPACE_BASE='rh-test' -p APP_NAME='api-client' \
     -p APP_GIT_URL='https://gogs-cicd-tools.cloud.sfb/banestes/api-cliente.git' \
     -p JENKINS_FILE='pipelines/Jenkinsfile-Build' \
+    -p BRANCH='develop'
     | oc apply -n rh-test-dev -f -
 
 oc process -f build-configs/build-pipeline.yml \
     -p NAMESPACE_BASE='rh-test' -p APP_NAME='api-client' \
     -p APP_GIT_URL='https://gogs-cicd-tools.cloud.sfb/banestes/api-cliente.git' \
     -p JENKINS_FILE='pipelines/Jenkinsfile-Build' \
+    -p BRANCH='none'
     | oc apply -n rh-test-hml -f -
 
 oc process -f build-configs/build-pipeline.yml \
     -p NAMESPACE_BASE='rh-test' -p APP_NAME='api-client' \
     -p APP_GIT_URL='https://gogs-cicd-tools.cloud.sfb/banestes/api-cliente.git' \
     -p JENKINS_FILE='pipelines/Jenkinsfile-Build' \
+    -p BRANCH='master'
     | oc apply -n rh-test -f -

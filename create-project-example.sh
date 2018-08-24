ed90e91110e0d45a953bcaea6786edb01473bf60
David Farias <dfarias@redhat.com>
2018-08-24 17:11:54 -0300

    New updates

--- create-project-example.sh
+++ create-project-example.sh
@@ -20,10 +20,10 @@ source ./create-app-fis.sh
 
 APP_NAME=eap-app-rest
 APP_GIT_URL='https://gogs-cicd-tools.cloud.sfb/banestes/eap-app-rest.git'
-CONFIGMAP_URL_DEV='https://gogs-cicd-tools.cloud.sfb/banestes/eap-app-rest/raw/master/configuration/configmap/eap-app-rest.yml  '
+CONFIGMAP_URL_DEV='https://${jenkins_user}:${jenkins_pwd}@gogs-cicd-tools.cloud.sfb/banestes/eap-app-rest/raw/master/configuration/configmap/eap-app-rest.yml  '
 CONFIGMAP_URL_HML=$CONFIGMAP_URL_DEV
 CONFIGMAP_URL_PRD=$CONFIGMAP_URL_DEV
-SECRET_URL_DEV='https://gogs-cicd-tools.cloud.sfb/banestes/eap-app-rest/raw/master/configuration/secret/eap-app-rest.yml'
+SECRET_URL_DEV='https://${jenkins_user}:${jenkins_pwd}@gogs-cicd-tools.cloud.sfb/banestes/eap-app-rest/raw/master/configuration/secret/eap-app-rest.yml'
 SECRET_URL_HML=$SECRET_URL_DEV
 SECRET_URL_PRD=$SECRET_URL_DEV
 REPLICAS_HML=2
